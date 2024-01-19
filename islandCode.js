53cd2b78dd49c8adeaf526375201a8653a4e018e
CounterfeitBees <henriquesviolet@gmail.com>
2024-01-19 14:07:22 -0500

    reworked website structure, now has more iframe powers

---
+++ islandCode.js
@@ -0,0 +1,28 @@
+function setContent(newSrc) {
+  document.getElementById("content").src = newSrc;
+}
+
+function setNavAll(){
+  document.getElementById("sidebar").src = "Navbars/sidebar.html";
+}
+
+function setNavBlog(){
+  document.getElementById("sidebar").src = "Navbars/blogPosts.html";
+}
+
+function setNavDONE(){
+  document.getElementById("sidebar").src = "Navbars/projects.html";
+}
+
+function setNavJournal(){
+  document.getElementById("sidebar").src = "Navbars/journal.html";
+}
+
+function setNavNone() {
+  document.getElementById("sidebar").src = " ";
+}
+
+function setContentBlog() {
+  document.getElementById("link").innerHTML = "projects.html";
+  window.parent.document.getElementById('yourdiv').src = "projects.html";
+}
