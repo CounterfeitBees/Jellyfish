6782510a3d77fc497225a602446ebf1c87d8f859
CounterfeitBees <henriquesviolet@gmail.com>
2024-02-24 06:47:15 -0500

    FIRST TEST REORDER HIERARCHY

--- islandCode.js
+++ islandCode.js
@@ -1,35 +1,18 @@
+var sideBarContent = "Navbars/blogPosts.html"
+
+function setSideBarScr() {
+  document.getElementById("sidebar").src = sideBarContent;
+}
+
 function setContent(newSrc) {
   document.getElementById("content").src = newSrc;
 }
 
-function setNavAll(){
-  document.getElementById("sidebar").src = "Navbars/sidebar.html";
-}
-
-function setNavBlog(){
-  document.getElementById("sidebar").src = "Navbars/blogPosts.html";
-}
-
-function setNavDONE(){
-  document.getElementById("sidebar").src = "Navbars/projects.html";
-}
-
-function setNavScience(){
-  document.getElementById("sidebar").src = "Navbars/science.html";
-}
-
-function setNavNone() {
-  document.getElementById("sidebar").src = " ";
+function setSidebar(newSrc){
+  sideBarContent = newSrc;
+  setSideBarScr();
 }
 
 function setContentBlog(newSrc) {
   window.parent.document.getElementById("content").src = newSrc;
-}
-
-function giveRandomColor() {
-  return(Math.floor(Math.random() * 256)-1);
-}
-
-function funColors() {
-
 }
