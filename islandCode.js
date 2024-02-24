67b858de72c465d8be817dd78ad76896d3fecb63
CounterfeitBees <henriquesviolet@gmail.com>
2024-02-24 10:36:20 -0500

    test?

--- islandCode.js
+++ islandCode.js
@@ -1,4 +1,4 @@
-localStorage.setItem("sidebarContent", '/Jellyfish/Navbars/blogPosts.html');
+//localStorage.setItem("sidebarContent", '/Jellyfish/Navbars/blogPosts.html');
 
 function setSidebarSrc(newSrc) {
   document.getElementById("sidebar").src = newSrc;
@@ -8,10 +8,10 @@ function setContent(newSrc) {
   document.getElementById("content").src = newSrc;
 }
 
-function setSidebar(newSrc){
-  localStorage.setItem("sidebarContent", newSrc);
-  setSidebarSrc(localStorage.getItem("sidebarContent"));
-}
+// function setSidebar(newSrc){
+//   localStorage.setItem("sidebarContent", newSrc);
+//   setSidebarSrc(localStorage.getItem("sidebarContent"));
+// }
 
 function setContentBlog(newSrc) {
   window.parent.document.getElementById("content").src = newSrc;
